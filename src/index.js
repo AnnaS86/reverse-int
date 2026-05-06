58494af99f0f490b63b4c263db21e79b3afcf732
Anna Sadovaia <lady.sadovaya@gmail.com>
2026-05-06 21:32:58 +0500

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const modul = Math.abs(n);
+  const result = Array.from(String(modul)).reverse().join('');
+  return Number(result);
 };
